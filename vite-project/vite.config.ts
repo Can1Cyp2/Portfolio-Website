60e7313d3a36ed6283ae89eca092bd002deefd36
Sebastian L <99sebastianl@gmail.com>
2024-09-07 14:45:46 -0400

    Revert "Updated App.tsx for git pages'"
    This reverts commit bc3b66c5663bfb0ece0853ae9524a51686bdb2fc.
    
    git status
    git diff --staged

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Website/", // GitHub repository name
+  base: "/Portfolio-Website/",
   plugins: [react()],
 });
