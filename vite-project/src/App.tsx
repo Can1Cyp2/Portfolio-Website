bc3b66c5663bfb0ece0853ae9524a51686bdb2fc
Sebastian L <99sebastianl@gmail.com>
2024-09-07 14:29:17 -0400

    Updated App.tsx for git pages'

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Use HashRouter
 import TitlePage from "./components/TitlePage";
 import ProjectsSection from "./components/ProjectsSection";
 import ContactPage from "./contactPage/ContactPage";
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Website/",
+  base: "/Portfolio-Website/", // GitHub repository name
   plugins: [react()],
 });
