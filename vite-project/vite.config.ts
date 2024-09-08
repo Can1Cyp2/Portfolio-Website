bb4aae4542e6d0d7fe0b3739f78696c2316ae405
Sebastian L <99sebastianl@gmail.com>
2024-09-07 23:58:19 -0400

    Committing local changes before pull

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio-Website/", // Your GitHub repository name
   plugins: [react()],
+  base: "/Portfolio-Website/", // Your GitHub repository name
 });
