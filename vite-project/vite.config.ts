7c3d8f05e3a2dc9b46dc8ee08be187e4e5b60110
Sebastian L <99sebastianl@gmail.com>
2025-02-05 20:46:11 -0500

    Fixed a small issue where text would not fully show up on certain mobile devices when viewing the Poetry Backend Section on the projects page

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Website/", // Your GitHub repository name
+  base: "/Portfolio-Website/",
 });
