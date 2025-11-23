a3014961c44694979b6ccd4b4c0e45cfd88bcebf
Sebastian L <99sebastianl@gmail.com>
2025-11-23 14:13:23 -0500

    Corrected last edited date

--- vite-project/src/components/Footer.tsx
+++ vite-project/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>Created By Sebastian in August 2024 - Last Edited: 2025-07-10</p>
+        <p>Created By Sebastian in August 2024 - Last Edited: 2025-11-23</p>
         <p>Email: 99SebastianL@gmail.com | Phone: (647)-575-3734</p>
       </div>
     </footer>
